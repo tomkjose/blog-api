01b6cb4b5dcd631c87548bdd15568f8fa1300e8b
tomkjose <tomjosekunnumpurath@gmail.com>
2024-05-03 12:55:53 +0530

    Fix: DB error

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/BlogDB");
+require("dotenv").config();
+const BlogDB = process.env.DB;
+mongoose.connect(BlogDB);
 const db = mongoose.connection;
 
 db.on(
